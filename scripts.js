9e1a794a2949a22f0870a00179ee264b698f06e8
savioHub <saviosantossilva19@gmail.com>
2022-06-10 09:48:06 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+const nome = 'savio santos da silva'
+
+let idade = 18
+let maiorIdade
+
+if (idade >= 18) {
+    maiorIdade = true
+    if (maiorIdade == true) {
+        console.log(nome + ' é maior de ' + idade + ' anos.')
+    }else{
+        maiorIdade = false
+    }
+}
