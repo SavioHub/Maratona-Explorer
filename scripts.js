62618daece0d203a146822d72e2d6a8803daf4b5
savioHub <saviosantossilva19@.com>
2022-06-14 08:35:25 -0300

    iniciando cod do script

--- scripts.js
+++ scripts.js
@@ -38,12 +38,7 @@ const respostas =[
     "A meu ver, sim.",
     "Minhas fontes dizem não.",
     "Provavelmente.",
-    "Não é possível prever agora.",
-    "Perspectiva boa.",
-    "As perspectivas não são tão boas.",
-    "Sim.",
-    "Concentre-se e pergunte novamente.",
-    "Sinais apontam que sim.",
+ 
 ]
 
 //gerar numeros aleattorios
